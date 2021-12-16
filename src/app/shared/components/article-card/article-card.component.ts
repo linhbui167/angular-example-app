517596b8c362cde70e77d85eb3e1cb4093650403
Linh Bui <linh.cn3@gmail.com>
2021-12-16 16:49:23 +0700

    Create article-card.component.ts

---
+++ src/app/shared/components/article-card/article-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, Input } from '@angular/core';
+import { Article } from '../../../modules/article/shared/article.model';
+import { transition, trigger, useAnimation } from '@angular/animations';
+import { fadeIn } from 'ng-animate';
+import { ROUTES_CONFIG, RoutesConfig } from '../../../configs/routes.config';
+
+@Component({
+  selector: 'app-article-card',
+  templateUrl: './article-card.component.html',
+  styleUrls: ['./article-card.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(
+        '* => *',
+        useAnimation(fadeIn, {
+          params: { timing: 1, delay: 0 },
+        })
+      ),
+    ]),
+  ],
+})
+export class ArticleCardComponent {
+  @Input() article: Article | undefined;
+
+  constructor(@Inject(ROUTES_CONFIG) public routesConfig: any) {}
+}
