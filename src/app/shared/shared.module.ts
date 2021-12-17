a1f2804206d23116f3362fd40be4e82f181684b0
Linh Bui Dinh <linh.bui@luxstay.com>
2021-12-17 23:01:09 +0700

    update: yarnlock

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { SpinnerComponent } from './components/spinner/spinner.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxExampleLibraryModule } from '@ismaestro/ngx-example-library';
-import { NgxScrollToFirstInvalidModule } from '@ismaestro/ngx-scroll-to-first-invalid';
 import { LoadingPlaceholderComponent } from './components/loading-placeholder/loading-placeholder.component';
 import { CapitalizeFirstPipe } from './pipes/capitalize-first.pipe';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
@@ -24,7 +23,6 @@ import { BackToTopComponent } from './components/back-to-top/back-to-top.compone
     ReactiveFormsModule,
     RouterModule,
     NgxExampleLibraryModule,
-    NgxScrollToFirstInvalidModule,
     LazyLoadImageModule,
   ],
   declarations: [
@@ -43,7 +41,6 @@ import { BackToTopComponent } from './components/back-to-top/back-to-top.compone
     FlexLayoutModule,
     NgxExampleLibraryModule,
     SpinnerComponent,
-    NgxScrollToFirstInvalidModule,
     LoadingPlaceholderComponent,
     CapitalizeFirstPipe,
     LazyLoadImageModule,
