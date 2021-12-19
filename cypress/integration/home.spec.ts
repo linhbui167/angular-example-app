97d32ac61cb8453ffb99c71d234ea234352eb495
Linh Bui Dinh <linh.bui@luxstay.com>
2021-12-19 22:23:10 +0700

    chore<home-page>: write test

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,8 +1,18 @@
 
 describe('Home page', () => {
-  
-  it('shoud display list article', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 800)
+  })
+  it('should change view', () => {
     cy.visit('/')
-
+    cy.get('.view-type').click()
+    cy.get('#article-list').should('have.class', 'is-row')
+    cy.get('.view-type').click()
+    cy.get('#article-list').should('have.class', 'is-grid')
+  })
+  it('should scroll to top', () => {
+    cy.scrollTo('bottom')
+    cy.get('.backtotop__wrap').click()
+    cy.window().its('scrollY').should('equal', 0)
   })
 })
